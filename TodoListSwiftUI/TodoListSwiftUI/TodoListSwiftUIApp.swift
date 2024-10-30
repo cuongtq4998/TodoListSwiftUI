295894fd9ac7f6255c895a6493c79b1fd09d0a0c
cuongtran <cuongtq4998@gmail.com>
2024-10-30 16:29:24 +0700

    init project

---
+++ TodoListSwiftUI/TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by cuongtran on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
